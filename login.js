31fbeb34b308c2eef2e093230c489cdba423cde1
sejalnama7 <sejalnama7@gmail.com>
2026-04-25 11:22:28 +0530

    updated validation and html code

--- login.js
+++ login.js
@@ -1,27 +1,12 @@
-function login() {
+ function validate() {
+            var user = document.getElementById("email").value;
+            var pass = document.getElementById("password").value;
 
-    let username = document.getElementById("username");
-    let password = document.getElementById("password");
+            if (user === "" || pass === "") {
+                alert("Please fill all the fields!");
+                return false;
+            }
 
-    username.style.border = "";
-    password.style.border = "";
-
-
-    if(username.value === ""){
-        username.style.border = "2px solid red";
-    }
-
-    if(password.value === ""){
-        password.style.border = "2px solid red";
-    }
-
-
-    else if(password.value.length < 7){
-        password.style.border = "2px solid red";
-        alert("Password must be at least 7 characters");
-    }
-
-    else if(username.value !== "" && password.value.length >= 7){
-        window.location.href = "welcome.html";
-    }
-}
+            window.location.href = "welcome.html";
+            return false; 
+        }
